ab4fa665c9ace1ff8891651e733cc255ba5a26dc
Sol Roberts <badger32d@gmail.com>
2014-01-24 21:18:02 -0700

    initial commit, skeleton framework

---
+++ common/__init__.py
@@ -0,0 +1,6 @@
+from json_handler import jsondump
+from jobs import Jobs
+from metrics import Metrics
+from settings import Settings
+from shellcmds import Command
+from ws import WS
---
+++ common/jobs.py
@@ -0,0 +1,15 @@
+from common import jsondump
+import logging
+
+
+class Jobs(object):
+    '''example of a jobs class'''
+    def __init__(self, main):
+        self.main = main
+
+    def a_job(self):
+        '''example of a job including a json conversion tool'''
+        info = ['list, of, info']
+        information = jsondump(info)
+        logging.info('info was converted to json-friendly format')
+        pass
